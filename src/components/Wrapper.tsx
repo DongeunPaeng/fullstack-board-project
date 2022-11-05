63f17dffb1e7c87b289330931f4fa3a5f6d15dc5
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-11-05 19:25:09 +0900

    update: 디자인 변경

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,5 +1,7 @@
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-5xl mx-auto px-6 pt-6 lg:px-8">{children}</div>;
+    return (
+        <div className="pb-14 max-w-5xl mx-auto px-6 pt-6 lg:px-8">{children}</div>
+    );
 };
 
 export default Wrapper;
