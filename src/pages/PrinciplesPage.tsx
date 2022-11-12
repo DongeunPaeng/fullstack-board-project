f81dd6f6336a2de51138041bfc2ff46d79cfff86
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-11-12 10:27:35 +0900

    principles updated

--- src/pages/PrinciplesPage.tsx
+++ src/pages/PrinciplesPage.tsx
@@ -2,16 +2,15 @@ import Greetings from "../components/Greetings";
 
 const principles = [
     "인생은 우주와의 조용한 대화이다.",
-    "남의 인생으로부터 감정 변화가 생기지 않게 해라.",
     "좋은 책을 끊임없이 읽어라.",
+    "남의 인생으로부터 감정 변화가 생기지 않게 해라.",
     "우주가 준 것에 감사하고, 주지 않은 것을 욕심내지 마라.",
     "옳은 생각은 외롭다.",
     "죽는 날이 반드시 오고, 나는 완전히 사라진다.",
-    "시기심의 원인을 파악해라.",
-    "단 한 번의 실수가 천 번의 성공을 죽인다.",
     "가장 가까운 사람들의 행복이 나의 행복이다.",
     "세상은 단기적으로 불합리하고, 장기적으로 단조롭다.",
     "걱정을 하느니 명상과 기도를 해라.",
+    "최고의 기업가가 되고 싶은가? 최고가 무엇이며 왜 최고이고 싶은가?",
 ];
 
 const PrinciplesPage = () => {
@@ -28,7 +27,7 @@ const PrinciplesPage = () => {
                     </li>
                 ))}
             </ul>
-            <div className="text-sm text-gray-400 mb-2">last updated: 2022-11-05</div>
+            <div className="text-sm text-gray-400 mb-2">last updated: 2022-11-12</div>
         </div>
     );
 };
