d2e3b8ef6854eb8e653509af2a23626f3293907c
Dongeun Paeng <dongeun.paeng@gmail.com>
2023-04-09 19:54:17 +0900

    fix: upgrade libraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    media: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
