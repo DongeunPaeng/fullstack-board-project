8141b112c98ee153858ef2fc92d45bea7b07c24c
Dongeun Paeng <dongeun.paeng@gmail.com>
2023-04-09 21:35:12 +0900

    test: changed logo

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -4,58 +4,58 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 
 const TopNavBar = () => {
-  const history = useHistory();
-  const { accessToken, setAccessToken, setUser } = useUser();
+    const history = useHistory();
+    const { accessToken, setAccessToken, setUser } = useUser();
 
-  const logout = () => {
-    axios.get("/api/logout").then((res) => {
-      if (res.status === 200) {
-        setAccessToken(null);
-        setUser(null);
-        history.push("/");
-      } else {
-        alert("something went wrong...");
-      }
-    });
-  };
+    const logout = () => {
+        axios.get("/api/logout").then((res) => {
+            if (res.status === 200) {
+                setAccessToken(null);
+                setUser(null);
+                history.push("/");
+            } else {
+                alert("something went wrong...");
+            }
+        });
+    };
 
-  return (
-    <nav>
-      <div>
-        <Link to="/" className="font-extrabold text-gray-800 items-center">
-          Dongeun Paeng
-        </Link>
-      </div>
-      <div className="mt-4 items-center sm:flex sm:justify-between">
-        <div className="block">
-          <Link className="topnavbar-links" to="/blogposts">
-            <span className="mr-4">Thoughts</span>
-          </Link>
-          <Link className="topnavbar-links" to="/study">
-            <span className="mr-4">StudyNotes</span>
-          </Link>
-          {accessToken ? (
-            <Link className="topnavbar-links" to="/draft">
-              <span className="mr-4 text-gray-400">Draft</span>
-            </Link>
-          ) : null}
-        </div>
-        {accessToken ? (
-          <div>
-            <Link className="topnavbar-links" to="/write">
-              <span className="mr-4">Write</span>
-            </Link>
-            <Link className="mr-4 topnavbar-links" to="/mypage">
-              MyPage
-            </Link>
-            <button className="topnavbar-links" onClick={logout}>
-              Logout
-            </button>
-          </div>
-        ) : null}
-      </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div>
+                <Link to="/" className="font-extrabold text-gray-800 items-center">
+                    TEST!! R U THERE?
+                </Link>
+            </div>
+            <div className="mt-4 items-center sm:flex sm:justify-between">
+                <div className="block">
+                    <Link className="topnavbar-links" to="/blogposts">
+                        <span className="mr-4">Thoughts</span>
+                    </Link>
+                    <Link className="topnavbar-links" to="/study">
+                        <span className="mr-4">StudyNotes</span>
+                    </Link>
+                    {accessToken ? (
+                        <Link className="topnavbar-links" to="/draft">
+                            <span className="mr-4 text-gray-400">Draft</span>
+                        </Link>
+                    ) : null}
+                </div>
+                {accessToken ? (
+                    <div>
+                        <Link className="topnavbar-links" to="/write">
+                            <span className="mr-4">Write</span>
+                        </Link>
+                        <Link className="mr-4 topnavbar-links" to="/mypage">
+                            MyPage
+                        </Link>
+                        <button className="topnavbar-links" onClick={logout}>
+                            Logout
+                        </button>
+                    </div>
+                ) : null}
+            </div>
+        </nav>
+    );
 };
 
 export default TopNavBar;
