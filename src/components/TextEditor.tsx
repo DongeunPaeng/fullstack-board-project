8f0cabe1fd3008bdbb2661b4f4289ee0a5b77019
Dongeun Paeng <dongeun.paeng@gmail.com>
2024-06-07 04:52:54 +0900

    minor fix

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -97,7 +97,7 @@ const TextEditor: React.FunctionComponent<TextEditorProps> = ({ post }) => {
           <div className="text-sm text-gray-500 mb-1">Post</div>
           <ReactQuill
             theme="snow"
-            className="h-full mb-10"
+            className="h-[90vh] mb-10"
             value={value || ""}
             onChange={setValue}
             modules={modules}
